2341c11c363e0ea4a7b08ebadb87ee6dd0d422fa
hubgitark <arket@outlook.com>
2022-04-01 22:28:21 +0800

    Update format & graph.py

--- format & graph.py
+++ format & graph.py
@@ -15,22 +15,40 @@ filePath = r'E:\OneDrive\NUS\CS5260\assignment 6/'
 os.chdir(filePath)
 
 
-text_file = open("colossalai_steplr.txt", "r")
+text_file = open("col_step_SGD.txt", "r")
 data = text_file.read()
 text_file.close()
  
-dataLst = data.split('100%')
+dataLst = data.split('/ Train')
 
-loss_lr_DF = pd.DataFrame(columns=['loss','lr'])
+loss_lr_DF_train = pd.DataFrame(columns=['loss','lr'])
 for i in range(len(dataLst)):
     try:
+        #i = 1
+        #str1 = dataLst[i]
         lossStr = float(dataLst[i].split('Loss = ')[1].split(' ')[0].strip())
         lrStr = float(dataLst[i].split('LR = ')[1].split('\n')[1].strip())
-        loss_lr_DF = loss_lr_DF.append({'loss':lossStr,'lr':lrStr},ignore_index=True)
+        loss_lr_DF_train = loss_lr_DF_train.append({'loss':lossStr,'lr':lrStr},ignore_index=True)
     except:
         continue
+    
+dataLst = data.split('/ Test')
+
+loss_lr_DF_test = pd.DataFrame(columns=['loss','lr'])
+for i in range(len(dataLst)):
+    try:
+        #i = 1
+        #str1 = dataLst[i]
+        lossStr = float(dataLst[i].split('Loss = ')[1].split(' ')[0].strip())
+        lrStr = float(dataLst[i].split('LR = ')[1].split('\n')[1].strip())
+        loss_lr_DF_test = loss_lr_DF_test.append({'loss':lossStr,'lr':lrStr},ignore_index=True)
+    except:
+        continue    
+    
     
 fig = plt.figure()
 ax = plt.axes()
 
-ax.plot(loss_lr_DF.lr, loss_lr_DF.loss)
+ax.plot(loss_lr_DF_train.lr, loss_lr_DF_train.loss)
+ax.plot(loss_lr_DF_test.lr, loss_lr_DF_test.loss)
+
